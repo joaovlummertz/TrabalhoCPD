46bbd85a3b9f9b4c0ed24a9e0eee5c91524565cf
joaovlummertz <joaolummertz1@gmail.com>
2025-01-03 18:28:52 -0300

    Data modeling

--- main.py
+++ main.py
@@ -3,12 +3,30 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 import main_window
 from main_window import Ui_MainWindow
 
+class Song:
+    def __init__(self, title, artist, year, total_streams, peak_daily, genre):
+        self.title = title
+        self.artist = artist
+        self.year = year
+        self.total_streams = total_streams
+        self.peak_daily = peak_daily
+        self.genre = genre
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Spotify Analytics")
-        self.setGeometry(0, 0, 500, 500)
+class Artist:
+    def __init__(self, name, country, sex, total_streams, most_streamed_song, songs):
+        self.name = name
+        self.country = country
+        self.sex = sex
+        self.total_streams = total_streams
+        self.most_streamed_song = most_streamed_song
+        self.songs = songs
+
+class Genre:
+    def __init__(self, name, total_streams, most_streamed_song, most_streamed_artist):
+        self.name = name
+        self.total_streams = total_streams
+        self.most_streamed_song = most_streamed_song
+        self.most_streamed_artist = most_streamed_artist
 
 def main():
     app = QApplication(sys.argv)
